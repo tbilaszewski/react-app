b6c1bddc93824eaf9dfdf7dabdffe7c5885fb40d
Tomasz Bilaszewski <tomasz.bilaszewski@gmail.com>
2020-02-02 15:45:47 +0100

    cleanup

--- client/src/App/App.test.js
+++ client/src/App/App.test.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import Enzyme, { mount, shallow } from 'enzyme';
 import { Provider } from 'react-redux';
-import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() })
 
-import { ListGroup, Alert, Form } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
 
 import App from '.';
 import EventForm from '../components/EventForm';
-import SubmitInformation from '../components/SubmitInformation';
-import ValidationMessages from '../components/ValidationMessages';
-import thunk from 'redux-thunk';
-
-const middlewares = [thunk];
 
 
-const mockStore = configureStore(middlewares);
+const mockStore = configureStore([]);
 
 it('renders without crashing', () => {
   shallow(<App />);
